0204d87e48bd1200240671af38e610268f77e27f
Farhan Athaullah <athaullahfarhan@gmail.com>
2021-03-25 21:52:58 +0700

    Adding on ready event

--- Cogs/Commands.py
+++ Cogs/Commands.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Commands cog has been loaded.")
 
 def setup(bot):
     bot.add_cog(Commands(bot))
